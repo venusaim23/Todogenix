28ce2f24341455fdf9c19d82353c014b4848068a
Venu Sai <venusaim23@gmail.com>
2021-12-10 15:12:30 +0530

    add Alan button

--- Android/app/src/main/java/com/alanai/todogenix/MainActivity.java
+++ Android/app/src/main/java/com/alanai/todogenix/MainActivity.java
@@ -2,34 +2,72 @@ package com.alanai.todogenix;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
+import com.alan.alansdk.AlanCallback;
+import com.alan.alansdk.AlanConfig;
+import com.alan.alansdk.events.EventCommand;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
-import android.view.View;
+import android.util.Log;
 
 import com.alanai.todogenix.databinding.ActivityMainBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    private HomeFragment homeFragment;
+    private TodoFragment todoFragment;
+    private FragmentManager manager;
+
+    private static final String SDK_KEY = "f5e57554206244ac1dc9a0d5cd74e85d2e956eca572e1d8b807a3e2338fdd0dc/stage";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        homeFragment = new HomeFragment();
+        todoFragment = new TodoFragment();
+        manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.frame_layout_main, homeFragment).commit();
+
+        AlanConfig config = AlanConfig.builder().setProjectId(SDK_KEY).build();
+        binding.alanButton.initWithConfig(config);
+
+        AlanCallback alanCallback = new AlanCallback() {
+            /// Handle commands from Alan Studio
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onCommand(final EventCommand eventCommand) {
+                try {
+                    JSONObject command = eventCommand.getData();
+//                    Log.d("JSONObject", "Basic Object: " + command);
+                    JSONObject data = command.getJSONObject("data");
+//                    Log.d("JSONObject", "Data Object: " + data);
+                    String commandName = data.getString("command");
+//                    Log.d("AlanButton", "onCommand: commandName: " + commandName);
+                } catch (JSONException e) {
+                    Log.e("AlanButton", e.getMessage());
+                }
             }
-        });
+        };
+
+        binding.alanButton.registerCallback(alanCallback);
+
+//        binding.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
 //    @Override
