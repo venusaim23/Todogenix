a6b4944555d1c9b16d4dc5d7024d96d2b160e9f6
Venu Sai <venusaim23@gmail.com>
2021-12-12 02:38:03 +0530

    update greeting messages

--- Android/app/src/main/java/com/alanai/todogenix/Fragments/HomeFragment.java
+++ Android/app/src/main/java/com/alanai/todogenix/Fragments/HomeFragment.java
@@ -3,6 +3,7 @@ package com.alanai.todogenix.Fragments;
 import android.content.Context;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +11,29 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.alanai.todogenix.Models.UserDetails;
+import com.alanai.todogenix.R;
 import com.alanai.todogenix.databinding.FragmentHomeBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeFragment extends Fragment {
 
     private HomeFragmentListener fragmentListener;
     private FragmentHomeBinding binding;
+
+    private Context context;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private DatabaseReference dbRef;
+
+    private UserDetails userDetails;
 
     public interface HomeFragmentListener {
         void openTodo();
@@ -26,8 +44,42 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        dbRef = FirebaseDatabase.getInstance().getReference().child(mUser.getUid());
+
+        getUserDetails();
+
         return binding.getRoot();
 
+    }
+
+    private void getUserDetails() {
+        dbRef.child("Details").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userDetails = snapshot.getValue(UserDetails.class);
+                Log.d("UserDetails", "Details: " + userDetails.getUid());
+                if (userDetails != null)
+                    updateDetails();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void updateDetails() {
+        if (userDetails.getName() != null) {
+            String greeting = context.getResources().getString(R.string.good_morning) + ",\n" +
+                    userDetails.getName() + context.getResources().getString(R.string.sun_emote);
+            binding.greetingTv.setText(greeting);
+        }
+
+        //todo sharedprefs
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -59,6 +111,7 @@ public class HomeFragment extends Fragment {
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
+        this.context = context;
         if (context instanceof HomeFragmentListener)
             fragmentListener = (HomeFragmentListener) context;
         else
